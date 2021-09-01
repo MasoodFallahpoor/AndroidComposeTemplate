09e0224d2124197f6feb8791a4be767a63cfe5de
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2021-09-01 09:42:53 +0200

    Upgrade Compose to 1.0.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,12 +1,12 @@
 object Dependencies {
 
-    internal const val kotlinVersion = "1.5.10"
-    internal const val composeVersion = "1.0.0-beta09"
+    internal const val kotlinVersion = "1.5.21"
+    internal const val composeVersion = "1.0.1"
 
     object App {
         const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}"
         const val appCompat = "androidx.appcompat:appcompat:1.3.0"
-        const val activityCompose = "androidx.activity:activity-compose:1.3.0-beta02"
+        const val activityCompose = "androidx.activity:activity-compose:1.3.0"
         const val material = "com.google.android.material:material:1.4.0-rc01"
         const val composeUi = "androidx.compose.ui:ui:$composeVersion"
         const val composeUiTooling = "androidx.compose.ui:ui-tooling:$composeVersion"
